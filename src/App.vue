59b42d2f95a1ebe5fa9a03e8ee28e0f3b3eaa16e
hhm <646973786@qq.com>
2022-03-22 00:39:33 +0800

    resize

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,21 @@
 <template>
-  <el-container id="app" class="whole_page">
-    <el-main>
+  <div id="app" class="whole_page" direction="vertical">
+    <el-row>
       <router-view></router-view>
-    </el-main>
-    <el-main class="header_page">
-      <el-menu default-active="1" class="menu" mode="horizontal" active-text-color="#ff0" router=true>
+    </el-row>
+    <el-row class="header_page">
+      <el-menu default-active="home" class="menu" :class="page" mode="horizontal" active-text-color="#ff0" router=true @select="select_page">
         <el-row class="header_menu">
-          <el-col :span="5"><div class="contents_1" style="text-align: left!important;">鸽已永治制作组</div></el-col>
-          <el-col :span="3"><el-menu-item class="contents" index="home" route="/">首页</el-menu-item></el-col>
-          <el-col :span="3"><el-menu-item class="contents" index="intro" route="/intro">人物简介</el-menu-item></el-col>
-          <el-col :span="3"><el-menu-item class="contents" index="work" route="/work">攻略集锦</el-menu-item></el-col>
-          <el-col :span="3"><el-menu-item class="contents" index="staff" route="/staff">STAFF</el-menu-item></el-col>
-          <el-col :span="3"><el-menu-item class="contents" index="QA" route="/QA">Q&A</el-menu-item></el-col>
+          <el-col :span="5"><div class="contents_1 home_group_info" disabled="true">鸽已永治制作组</div></el-col>
+          <el-col :span="3"><el-menu-item class="contents btn_home" index="home" route="/"></el-menu-item></el-col>
+          <el-col :span="3"><el-menu-item class="contents btn_intro" index="intro" route="/intro"></el-menu-item></el-col>
+          <el-col :span="3"><el-menu-item class="contents btn_work" index="work" route="/work"></el-menu-item></el-col>
+          <el-col :span="3"><el-menu-item class="contents btn_staff" index="staff" route="/staff"></el-menu-item></el-col>
+          <el-col :span="3"><el-menu-item class="contents btn_QA" index="QA" route="/QA"></el-menu-item></el-col>
         </el-row>
       </el-menu>
-    </el-main>
-  </el-container>
+    </el-row>
+  </div>
 </template>
 
 <script>
@@ -51,9 +51,10 @@ body {
 
 .whole_page {
   background-image: url("./assets/menu/img_封面拉伸底图.png");
+  background-size: contain;
   background-repeat: no-repeat;
   text-align: center;
-  width: 1920px;
+  width: 100%;
 }
 
 .header_page {
@@ -62,53 +63,83 @@ body {
   padding: 0!important;
   position: absolute;
   top: 0;
+  left: 0;
+  right: 0;
   margin: auto;
-  width: 1920px;
+  width: 100%;
+  max-width: 1920px;
+}
+
+.home_group_info {
+  height: 56px;
+  line-height: 56px;
+  font-size: 18pt;
+  color: #ffffff;
+  font-family: '宋体';
+  white-space: nowrap;
+  text-align: left;
+  opacity: 0.8;
+}
+
+.btn_home {
+  background-image: url("./assets/menu/btn_首页.png");
+}
+
+.btn_home:hover, .home .btn_home {
+  background-image: url("./assets/menu/img_首页.png");
+}
+
+.btn_intro {
+  background-image: url("./assets/menu/btn_人物简介.png");
+}
+
+.btn_intro:hover, .intro .btn_intro {
+  background-image: url("./assets/menu/img_人物简介.png");
+}
+
+.btn_work {
+  background-image: url("./assets/menu/btn_攻略集锦.png");
+}
+
+.btn_work:hover, .work .btn_work {
+  background-image: url("./assets/menu/img_攻略集锦.png");
+}
+
+.btn_staff {
+  background-image: url("./assets/menu/btn_staff.png");
+}
+
+.btn_staff:hover, .staff .btn_staff {
+  background-image: url("./assets/menu/img_staff.png");
+}
+
+.btn_QA {
+  background-image: url("./assets/menu/btn_qa.png");
+}
+
+.btn_QA:hover, .QA .btn_QA {
+  background-image: url("./assets/menu/img_qa.png");
 }
 
 .contents, .contents_1 {
-  height: 2cm;
-  line-height: 2cm !important;
-  font-size: 18pt;
-  color: #fff;
   margin: 1px 5px;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+}
+
+.contents:hover, .contents:focus {
+  background-color: transparent;
 }
 
 .contents_1 {
   padding: 0 20px;
 }
 
-.contents:focus, .contents:hover {
-  color: #ff0!important;
-  background-color: rgba(255, 255, 255, 0.4);
-}
-
-.submenu_title {
-  font-size: 20pt;
-  border-radius: 8px;
-}
-
-.sub_contents {
-  font-size: 20pt;
-  text-align: center;
-}
-
-.sub_contents:hover {
-  background-color: #ecf5ff !important;
-}
-
 .menu {
   width: inherit;
   border-bottom: 0 !important;
   background: transparent;
-}
-
-.el-submenu__icon-arrow {
-  display: none;
-}
-
-.el-submenu__title:hover {
-  border-radius: 8px;
 }
 
 .button_small {
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,11 +1,9 @@
 <template>
   <el-container class="page_home" direction="vertical">
     <el-main class="page_home_header">
-      <el-row style="height: 900px">
-      </el-row>
-      <el-row type="flex" justify="center">
+      <el-row type="flex" justify="center" class="play_button_row">
         <el-col :span="6">
-          <el-button class="play_button_1">
+          <el-button class="play_button_1" size="medium">
 
           </el-button>
         </el-col>
@@ -18,18 +16,18 @@
     </el-main>
     <el-main>
       <el-row>
-        <img src="../assets/home/img_游戏PV.png">
+        <img src="./home/img_游戏PV.png" class="home_img_1">
       </el-row>
       <el-row>
-        <img src="../assets/home/img_视频位置.png">
+        <img src="./home/img_视频位置.png" class="home_img_2">
       </el-row>
       <el-row>
         <div class="intro_text">
           {{ home_intro_text }}
         </div>
       </el-row>
-      <el-row style="margin-top: 100px; margin-bottom: 50px">
-        <img src="../assets/home/img_实机展示.png">
+      <el-row style="margin-top: 2%;">
+        <img src="./home/img_实机展示.png" class="home_img_1">
       </el-row>
       <el-row type="flex" justify="center">
         <el-col :span="8">
@@ -45,14 +43,14 @@
       </el-row>
       <el-row class="screenshot_base" type="flex" justify="center" align="center">
         <el-col :span="2">
-          <el-button class="select_button_1">
+          <el-button class="select_button select_button_1">
           </el-button>
         </el-col>
-        <el-col :span="12">
-          <img src="../assets/home/img_游戏截图.png">
+        <el-col :span="18">
+          <img src="./home/img_游戏截图.png" class="home_img_3">
         </el-col>
         <el-col :span="2">
-          <el-button class="select_button_1">
+          <el-button class="select_button">
           </el-button>
         </el-col>
       </el-row>
@@ -74,23 +72,16 @@
 <script>
 export default {
   methods: {
-    makeActive: function (item) {
-      this.active = item
-    },
-    play_avg () {
-      this.show_alert()
-    },
-    play_rpg () {
-      this.show_alert()
-    },
-    show_alert () {
-      this.$message('还没写好这个功能')
-    }
+
   },
   data () {
     return {
       home_intro_text: 'PV简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介简介',
-      authorization_text: '这是授权信息。'
+      authorization_text: '这是授权信息。',
+      pic_avg: {
+        0: './home/img_游戏截图.png',
+        1: './home/img_视频位置.png'
+      }
     }
   }
 }
@@ -98,56 +89,81 @@ export default {
 
 <style>
 .page_home {
-  background-image: url("../assets/home/img_底图拉伸自适应.png");
-  background-size: 100%;
+  background-image: url("./home/img_底图拉伸自适应.png");
+  background-size: contain;
+  max-width: 1920px;
+  margin: auto;
+}
+
+.el-button:focus {
+  background-color: transparent;
 }
 
 .page_home_header {
-  background-image: url("../assets/home/img_封面图.png");
+  background-image: url("./home/img_封面图.png");
   background-size: 100%;
   background-repeat: no-repeat;
-  height: 1080px;
 }
 
 .page_home_footer {
-  background-image: url("../assets/home/img_底部栏底.png");
+  background-image: url("./home/img_底部栏底.png");
   background-size: 100%;
 }
 
+.play_button_row {
+  margin: 45% 0 1% 0;
+}
+
 .play_button_1 {
-  background: url("../assets/home/btn_AVG游玩.png");
+  background-image: url("./home/btn_AVG游玩.png");
   background-size: 100%;
-  width: 392px;
-  height: 106px;
+  background-repeat: no-repeat;
+  background-color: transparent;
+  background-position: center;
+  width: 80%;
+  padding: 25% 0;
   border: 0;
 }
 
 .play_button_1:hover {
-  background: url("../assets/home/btn_AVG游玩.png");
-  background-size: 100%;
-  width: 411px;
-  height: 111px;
+  background-color: transparent;
+  width: 84%;
 }
 
 .play_button_2 {
-  background: url("../assets/home/btn_RPG下载.png");
+  background-image: url("./home/btn_RPG下载.png");
   background-size: 100%;
-  width: 392px;
-  height: 106px;
+  background-repeat: no-repeat;
+  background-color: transparent;
+  background-position: center;
+  width: 80%;
+  padding: 25% 0;
   border: 0;
 }
 
 .play_button_2:hover {
-  background: url("../assets/home/btn_RPG下载.png");
-  background-size: 100%;
-  width: 411px;
-  height: 111px;
+  background-color: transparent;
+  width: 84%;
+}
+
+.home_img_1 {
+  width: 40%;
+  margin-bottom: 2%;
+}
+
+.home_img_2 {
+  width: 60%;
+  margin-bottom: 2%;
+}
+
+.home_img_3 {
+  width: 100%;
+
 }
 
 .intro_text {
-  width: 70%;
+  width: 80%;
   margin: auto;
-  margin-top: 50px;
   font-size: 26px;
   color: #ffffff;
   font-family: '宋体';
@@ -161,53 +177,71 @@ export default {
 }
 
 .small_button_1 {
-  height: 74px;
-  width: 474px;
-  background: url("../assets/home/img_AVG游戏.png");
-  background-size: 100%;
+  width: 80%;
+  padding-bottom: 15%;
+  background-image: url("./home/img_AVG游戏.png");
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: center;
+  background-color: transparent;
   border: 0;
 }
 
-.small_button_1:hover, .small_button_1:active {
-  height: 74px;
-  width: 474px;
-  background: url("../assets/home/btn_AVG游戏.png");
-  background-size: 100%;
+.small_button_1:hover, .small_button_1:active, .small_button_1:focus {
+  background-image: url("./home/btn_AVG游戏.png");
+  background-color: transparent;
   border: 0;
 }
 
 .small_button_2 {
-  height: 74px;
-  width: 474px;
-  background: url("../assets/home/img_RPG游戏.png");
-  background-size: 100%;
+  width: 80%;
+  padding-bottom: 15%;
+  background-image: url("./home/img_RPG游戏.png");
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: center;
+  background-color: transparent;
   border: 0;
 }
 
-.small_button_2:hover, .small_button_2:active {
-  height: 74px;
-  width: 474px;
-  background: url("../assets/home/btn_RPG游戏.png");
-  background-size: 100%;
+.small_button_2:hover, .small_button_2:active, .small_button_2:focus {
+  background-image: url("./home/btn_RPG游戏.png");
+  background-color: transparent;
   border: 0;
 }
 
 .screenshot_base {
-  background-image: url("../assets/home/img_截图底图.png");
+  background-image: url("./home/img_截图底图.png");
+  background-size: 91%;
   background-repeat: no-repeat;
-  background-position: center top;
-  height: 526px;
-  margin-top: 50px;
-  margin-bottom: 100px;
+  background-position: center;
+  width: 70%;
+  margin: 2.5% auto 5% auto;
 }
 
-.select_button_1 {
-  height: 540px;
-  background: url("../assets/home/btn_切换箭头.png");
+.select_button {
+  margin: 180% 0;
+  padding: 100% 0 50% 0;
+  width: 80%;
+  background-image: url("./home/btn_切换箭头.png");
   background-size: 100%;
   background-repeat: no-repeat;
   background-position: left;
+  background-color: transparent;
   border: 0;
+}
+
+.select_button:focus, .select_button:active {
+  background-color: transparent;
+}
+
+.select_button:hover {
+  width: 84%;
+  background-color: transparent;
+}
+
+.select_button_1 {
+  transform: rotate(180deg);
 }
 
 </style>
